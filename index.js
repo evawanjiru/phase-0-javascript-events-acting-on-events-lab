cb8de9356479e639628738ba6e9b57f88226f6dd
evawanjiru <eva.wanjiru@student.moringaschool.com>
2024-12-18 09:49:07 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+// Function to move the dodger to the left
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10); 
+
+  if (left > 0) { 
+    dodger.style.left = `${left - 1}px`; 
+  }
+}
+
+// Function to move the dodger to the right
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", ""); 
+    const left = parseInt(leftNumbers, 10); 
+    if (left < 360) { 
+      dodger.style.left = `${left + 1}px`; 
+    }
+  }
